0d31f4631da161ab68d998b293cf4d4b1f1eef08
Elena Andreeva <elena@madebyforward.com>
2021-10-31 12:48:23 +0300

    Add css stylesheet, apply classes to components, create Button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,32 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
-
+// destructure of the props object
+// inline styling:
+// <h1 style={{ color: 'red', backgroundColor: 'black' }}>Hello</h1>
+// or
+// <h1 style={headingStyle}>Hello</h1>
 const Header = ({title}) => {
     return (
-        <header>
+        <header className='header'>
             <h1>{title}</h1>
+            <Button color='green' text='Hello'/>
         </header>
     )
 }
-
+// set the default value of the props object property of this component
 Header.defaultProps = {
     title: 'Task Tracker',
 }
-
+// check the value type and make the title property required (but the set up default value backs it up)
 Header.propTypes = {
     title: PropTypes.string.isRequired,
 }
+
+// object with css properties to be used for inline styles = CSS in JS
+// const headingStyle = {
+//     color: "red",
+//     backgroundColor: "black"
+// }
 
 export default Header
